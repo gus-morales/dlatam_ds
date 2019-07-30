17adfaab3f748076668035717325b3ea7521f65f
gus <gustavo85@gmail.com>
2019-07-30 03:59:57 -0400

    Even more files

--- desafios/emprendedor3.py
+++ desafios/emprendedor3.py
@@ -5,7 +5,7 @@ Keyword arguments:
 price_per_user
 total_users
 expenses
-profit_previous_year
+profit_prev_year
 """
 from os import sys
 
@@ -13,19 +13,19 @@ price_per_user = float(sys.argv[1])
 total_users = float(sys.argv[2])
 expenses = float(sys.argv[3])
 
-profit_previous_year = 1000
+profit_prev_year = 1000
 if len(sys.argv) == 5:
-    profit_previous_year = float(sys.argv[4])
+    profit_prev_year = float(sys.argv[4])
 TAXES = 0.35
 
 income = price_per_user * total_users
-profit_current_year = income - expenses
+profit_cur_year = income - expenses
 
-if profit_current_year > 0:
-    profit_current_year *= (1 - TAXES)
+if profit_cur_year > 0:
+    profit_cur_year *= (1 - TAXES)
 
-#rate_percent = profit_current_year * 100 / profit_previous_year
-rate_percent = profit_previous_year * 100 / profit_current_year
+rate_percent = profit_cur_year * 100 / profit_prev_year
+rate_percent = profit_prev_year * 100 / profit_cur_year
 
 print("")
 print("Utilidades actuales vs. año anterior = {}%".format(rate_percent))
