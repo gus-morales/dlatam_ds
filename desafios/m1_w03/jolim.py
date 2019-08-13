94e478a0e472051cf0ddfd328b4a10e07ead89bb
gus <gustavo85@gmail.com>
2019-08-12 21:02:19 -0400

    Even more files

--- desafios/m1_w03/jolim.py
+++ desafios/m1_w03/jolim.py
@@ -1,23 +1,51 @@
 import pandas as pd
+import numpy as np
 
 filename = "athlete_events.csv"
 
 df = pd.read_csv(filename)
-print(df.columns)
+
 # cantidad de registros y de columnas
 ejercicio_1 = df.shape
+#print(ejercicio_1)
+
 
 # total competencias (juegos)
 ejercicio_2 = df.Games.nunique()
-
-# % atletas que participaron tanto en los juegos de invierno como de verano
-total_ath = df.ID.nunique()
-print(total_ath)
-
-for (colName, colData) in df.iteritems():
-   if colName == "Season":
-       print(colData.values)
+#print(ejercicio_2)
 
 
+# % atletas que participaron tanto en verano como en invierno
+ejercicio_3 = df["Season"].value_counts(normalize=True)
+#print(ejercicio_3)
 
-#ejercicio_3 = 
+
+# donde fue la primera celebracion de un juego de verano
+ejercicio_4 = df[df["Season"] == "Summer"]\
+    .sort_values(by="Year")[["City"]].iloc[0]
+#print(ejercicio_4)
+
+
+# donde fue la primera celebracion de un juego de invierno
+ejercicio_5 = df[df["Season"] == "Winter"]\
+    .sort_values(by="Year")[["City"]].iloc[0]
+#print(ejercicio_5)
+
+
+# los 10 primeros países con mayor cantidad de atletas participantes
+#df6 = df.groupby("NOC").count()[["ID"]].sort_values(by="ID", ascending=False)
+#ejercicio_6 = df6.head(10).index
+ejercicio_6 = df["NOC"].value_counts().head(10)
+#print(ejercicio_6)
+
+
+# % de medallas asignadas (oro, bronce, plata)
+df7 = df["Medal"].value_counts(normalize=True)
+ejercicio_7 = df7
+#print(ejercicio_7)
+
+
+# los países participantes en las primeras olimpiadas de verano
+df_sum = df[df["Season"] == "Summer"]
+ejercicio_8 = df_sum[df_sum["Year"] == df_sum["Year"].min()].NOC.unique()
+#print(ejercicio_8)
