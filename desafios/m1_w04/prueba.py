b27ff92b2212a44d2a942c116fa64dc3d5c863c4
gus <gustavo85@gmail.com>
2019-08-18 17:45:00 -0400

    Even more files

--- desafios/m1_w04/prueba.py
+++ desafios/m1_w04/prueba.py
@@ -1,23 +1,37 @@
+"""
+Test docstring.
+"""
 import requests
 import json
-from pprint import pprint
+# from pprint import pprint
 # from termcolor import colored
 from pygments import highlight
 from pygments.lexers import JsonLexer
 from pygments.formatters import TerminalFormatter
 from itertools import groupby
 
+
 # def cprint(string, fgc="yellow", bgc="on_grey"):
 #     print(colored(string, fgc, bgc))
 
 def jprint(json_object):
+    """
+    :param json_object:
+    """
     json_str = json.dumps(json_object, indent=4, sort_keys=True)
     print(highlight(json_str, JsonLexer(), TerminalFormatter()))
 
-api_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
-api_key = "Pus8ET6nx19S7j7SEYWuudnca4XzOCKebZFlOeph"
+
+API_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+API_KEY = "Pus8ET6nx19S7j7SEYWuudnca4XzOCKebZFlOeph"
+
 
 def request(url, api_key):
+    """
+    :param url:
+    :param api_key:
+    :return: response.text
+    """
     headers = {
         'Cache-Control': "no-cache",
         'Postman-Token': "62832560-55f9-4b81-afa6-b3e9be321e3c,\
@@ -28,7 +42,11 @@ def request(url, api_key):
                                 headers=headers, params=query_dict)
     return json.loads(response.text)
 
-def build_web_page(response_dict):
+
+def build_web_page(response_dictionary):
+    """
+    :param response_dictionary:
+    """
     html_ini = """<html>
 <head>
 </head>
@@ -38,20 +56,26 @@ def build_web_page(response_dict):
 </body>
 </html>\n"""
     html_mid = ''
-    for photos in response_dict['photos']:
+    for photos in response_dictionary['photos']:
         html_mid += "    <li><img src=\"{}\"></li>\n".format(photos['img_src'])
     html = html_ini + html_mid + html_end
     with open("output.html", "w") as file:
         file.write(html)
 
-def count_photos(response_dict):
+
+def count_photos(response_dictionary):
+    """
+    :param response_dictionary:
+    :return: out_dict
+    """
     cameras = []
-    for i in response_dict["photos"]:
+    for i in response_dictionary["photos"]:
         cameras.append(i["camera"]["name"])
     out_dict = dict((k, len(list(g))) for k, g in groupby(cameras))
     return out_dict
 
-response_dict = request(api_url, api_key)
+
+response_dict = request(API_URL, API_KEY)
 build_web_page(response_dict)
 
 jprint(count_photos(response_dict))
