8fba3af676425be481e0785402c16203a36ccb98
gus <gustavo85@gmail.com>
2020-04-28 00:06:39 -0400

    Even more files

--- desafios/m5_bigdata/d3/install.sh
+++ desafios/m5_bigdata/d3/install.sh
@@ -3,21 +3,24 @@ CYAN='\033[0;36m'
 LRED='\033[1;31m'
 NC='\033[0m' # No Color
 
-echo "${YELLOW}SCRIPT START${NC}"
+#echo "${YELLOW}SCRIPT START${NC}"
+echo "SCRIPT START"
 echo ""
-echo "${CYAN}(*) Install basic Python libraries?${NC} (y/n)"
+#echo "${CYAN}(*) Install basic Python libraries?${NC} (y/n)"
+echo "(*) Install basic Python libraries? (y/n)"
 while true; do
     read -p "" yn
     case $yn in
         [Yy]* ) echo "";\
-        sudo pip-3.6 install ipython pandas numpy scikit-learn;;
+        sudo pip-3.7 install ipython pandas numpy scikit-learn; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 echo ""
 
-echo "${CYAN}(0) Download source files from GitHub?${NC} (y/n)"
+#echo "${CYAN}(0) Download source files from GitHub?${NC} (y/n)"
+echo "(0) Download source files from GitHub? (y/n)"
 while true; do
     read -p "" yn
     case $yn in
@@ -32,30 +35,35 @@ while true; do
 done
 echo ""
 
-echo "${CYAN}(1) Generating mock data${NC}"
+#echo "${CYAN}(1) Generating mock data${NC}"
+echo "(1) Generating mock data"
 python3.7 src/1_simulate_data.py 1000 11238 dat/train_delivery_data
 python3.7 src/1_simulate_data.py 10000 42 dat/test_delivery_data
 echo ""
 
-echo "${CYAN}(2) Training models${NC}"
+#echo "${CYAN}(2) Training models${NC}"
+echo "(2) Training models"
 while true; do
     read -p "Proceed? (y/n) " yn
     case $yn in
         [Yy]* ) echo "";\
                 python3.7 src/2_train_models.py | tee candidate_models.txt;\
                 mv candidate_models.txt txt/.; break;;
-        [Nn]* ) echo "${LRED}SCRIPT ABORTED${NC}"; exit;;
+        #[Nn]* ) echo "${LRED}SCRIPT ABORTED${NC}"; exit;;
+        [Nn]* ) echo "SCRIPT ABORTED"; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 echo ""
 
-echo "${CYAN}(3) Estimating probabilities${NC}"
+#echo "${CYAN}(3) Estimating probabilities${NC}"
+echo "(3) Estimating probabilities"
 python3.7 src/3_predict_model.py | tee eval_pr.txt
 mv eval_pr.txt txt/.
 echo ""
 
-echo "${CYAN}(4.1) Exporting files to S3 bucket${NC}"
+#echo "${CYAN}(4.1) Exporting files to S3 bucket${NC}"
+echo "(4.1) Exporting files to S3 bucket"
 while true; do
     read -p "Proceed? (y/n) " yn
     case $yn in
@@ -68,8 +76,10 @@ while true; do
 done
 echo ""
 
-echo "${CYAN}(4.2) Zipping project for evaluation${NC}"
+#echo "${CYAN}(4.2) Zipping project for evaluation${NC}"
+echo "(4.2) Zipping project for evaluation"
 zip Gustavo_Morales_desafio3.zip src/* dat/* txt/* scr/*
 echo ""
 
-echo "${YELLOW}SCRIPT END${NC}"
+#echo "${YELLOW}SCRIPT END${NC}"
+echo "SCRIPT END"
